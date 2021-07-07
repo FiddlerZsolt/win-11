97b54120826f941dc88a3ff03c46029e05d47834
Zsolt Fiddler <zsoltan94@gmail.com>
2021-07-07 10:42:19 +0200

    Javascript comments

--- script/start-menu.js
+++ script/start-menu.js
@@ -1,10 +1,12 @@
 const startMenu = document.querySelector(".start-menu");
 const startBtn = document.getElementById("start-btn");
 
+// == Start menu open & close == //
 function startToggle() {
   startMenu.classList.toggle("active");
 }
 
+// == Clicking outside the start menu closes == //
 window.addEventListener("click", function (e) {
   if (startMenu.contains(e.target) === false) {
     if (startBtn.contains(e.target)) {
