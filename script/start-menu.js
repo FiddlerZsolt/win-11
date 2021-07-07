f9ed3be81e2420759862517bbdebc693a54c03c7
Zsolt Fiddler <zsoltan94@gmail.com>
2021-07-07 10:39:27 +0200

    Implement the clicking outside the start menu

--- script/start-menu.js
+++ script/start-menu.js
@@ -1,10 +1,16 @@
 const startMenu = document.querySelector(".start-menu");
+const startBtn = document.getElementById("start-btn");
 
 function startToggle() {
-  // if (startMenu.style.display === "none") {
-  //   startMenu.style.display = "block";
-  // } else {
-  //   startMenu.style.display = "none";
-  // }
   startMenu.classList.toggle("active");
 }
+
+window.addEventListener("click", function (e) {
+  if (startMenu.contains(e.target) === false) {
+    if (startBtn.contains(e.target)) {
+      console.log("NEM a start menün kattintottál de a gombon");
+    } else {
+      startMenu.classList.remove("active");
+    }
+  }
+});
